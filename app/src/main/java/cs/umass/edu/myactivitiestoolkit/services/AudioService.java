e406605cbd14d881cf2575f84180e16d5b05bf2c
Anthony Li <anthli1895@gmail.com>
2016-11-09 02:20:17 -0500

    Sending audio to server and the CSV for no speaker

--- app/src/main/java/cs/umass/edu/myactivitiestoolkit/services/AudioService.java
+++ app/src/main/java/cs/umass/edu/myactivitiestoolkit/services/AudioService.java
@@ -21,7 +21,7 @@ import edu.umass.cs.MHLClient.client.MessageReceiver;
  * each second, you will receive a buffer of 8000 samples. We have set the service up to compute
  * the spectrogram of the audio data for you and send it to the UI for visualization.
  * <br><br>
- *
+ * <p>
  * Assignment 4 : In this assignment, you will send the incoming audio buffer to the server.
  * In your Python script, you will do the entire processing and classification pipeline. In
  * {@link #onConnected()}, we have registered a {@link edu.umass.cs.MHLClient.client.MessageReceiver}
@@ -30,7 +30,6 @@ import edu.umass.cs.MHLClient.client.MessageReceiver;
  * the result to the user in an intuitive form.
  *
  * @author CS390MB
- *
  * @see MicrophoneRecorder
  * @see MicrophoneRecorder.MicrophoneListener
  * @see AudioBufferReading
@@ -42,118 +41,130 @@ import edu.umass.cs.MHLClient.client.MessageReceiver;
  */
 public class AudioService extends SensorService implements MicrophoneRecorder.MicrophoneListener {
 
-    /** Used during debugging to identify logs by class */
-    @SuppressWarnings("unused")
-    private static final String TAG = AudioService.class.getName();
+  /**
+   * Used during debugging to identify logs by class
+   */
+  @SuppressWarnings("unused")
+  private static final String TAG = AudioService.class.getName();
 
-    /** The sensor responsible for collecting audio data from the phone. */
-    private MicrophoneRecorder mMicrophoneRecorder;
+  /**
+   * The sensor responsible for collecting audio data from the phone.
+   */
+  private MicrophoneRecorder mMicrophoneRecorder;
 
-    @Override
-    protected void onServiceStarted() {
-        broadcastMessage(Constants.MESSAGE.AUDIO_SERVICE_STARTED);
+  @Override
+  protected void onServiceStarted() {
+    broadcastMessage(Constants.MESSAGE.AUDIO_SERVICE_STARTED);
+  }
+
+  @Override
+  protected void onServiceStopped() {
+    broadcastMessage(Constants.MESSAGE.AUDIO_SERVICE_STOPPED);
+  }
+
+  protected void registerSensors() {
+    mMicrophoneRecorder = MicrophoneRecorder.getInstance(this);
+
+    Log.d(TAG, "Starting microphone.");
+    mMicrophoneRecorder.registerListener(this);
+    mMicrophoneRecorder.startRecording();
+
+  }
+
+  protected void unregisterSensors() {
+    if (mMicrophoneRecorder != null) {
+      mMicrophoneRecorder.unregisterListener(this);
+      mMicrophoneRecorder.stopRecording();
     }
+  }
 
-    @Override
-    protected void onServiceStopped() {
-        broadcastMessage(Constants.MESSAGE.AUDIO_SERVICE_STOPPED);
-    }
-
-    protected void registerSensors() {
-        mMicrophoneRecorder = MicrophoneRecorder.getInstance(this);
-
-        Log.d(TAG, "Starting microphone.");
-        mMicrophoneRecorder.registerListener(this);
-        mMicrophoneRecorder.startRecording();
-
-    }
-
-    protected void unregisterSensors() {
-        if (mMicrophoneRecorder != null) {
-            mMicrophoneRecorder.unregisterListener(this);
-            mMicrophoneRecorder.stopRecording();
+  @Override
+  public void onConnected() {
+    mClient.registerMessageReceiver(new MessageReceiver(Constants.MHLClientFilter.SPEAKER_DETECTED) {
+      @Override
+      protected void onMessageReceived(JSONObject json) {
+        String speaker;
+        try {
+          JSONObject data = json.getJSONObject("data");
+          speaker = data.getString("speaker");
         }
-    }
-
-    @Override
-    public void onConnected() {
-        mClient.registerMessageReceiver(new MessageReceiver(Constants.MHLClientFilter.SPEAKER_DETECTED) {
-            @Override
-            protected void onMessageReceived(JSONObject json) {
-                String speaker;
-                try {
-                    JSONObject data = json.getJSONObject("data");
-                    speaker = data.getString("speaker");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    return;
-                }
-                // TODO: Send the speaker to the UI
-            }
-        });
-        super.onConnected();
-    }
-
-    @Override
-    protected int getNotificationID() {
-        return Constants.NOTIFICATION_ID.AUDIO_SERVICE;
-    }
-
-    @Override
-    protected String getNotificationContentText() {
-        return getString(R.string.audio_service_notification);
-    }
-
-    @Override
-    protected int getNotificationIconResourceID() {
-        return R.drawable.ic_mic_white_24dp;
-    }
-
-    /**
-     * Broadcasts spectrogram of audio data.
-     * @param spectrogram 2d array of values
-     */
-    public void broadcastSpectrogram(double[][] spectrogram) {
-        Intent intent = new Intent();
-        intent.putExtra(Constants.KEY.SPECTROGRAM, spectrogram);
-        intent.setAction(Constants.ACTION.BROADCAST_SPECTROGRAM);
-        LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this);
-        manager.sendBroadcast(intent);
-    }
-
-    /**
-     * Called when an audio buffer is received. We compute and visualize the spectrogram
-     * for you.
-     * <br><br>
-     *
-     * Your job is just to send the audio buffer to the server. You should wrap it using
-     * an {@link AudioBufferReading}. For the timestamp, you can pass in any arbitrary
-     * value, or the current system time. It doesn't matter since it won't be used on the
-     * other end, as we aren't live streaming the audio data.
-     *
-     * @param buffer the raw audio data
-     * @param window_size the size of the buffer
-     *
-     * @see MicrophoneRecorder
-     * @see cs.umass.edu.myactivitiestoolkit.audio.MicrophoneRecorder.MicrophoneListener
-     * @see AudioBufferReading
-     */
-    @Override
-    public void microphoneBuffer(short[] buffer, int window_size) {
-        Log.d(TAG, String.valueOf(buffer.length));
-
-        //TODO: Send the audio buffer to the server
-
-        //convert short[] to double[] for computing spectrogram
-        double[] dBuffer = new double[buffer.length];
-        for (int j=0;j<buffer.length;j++) {
-            dBuffer[j] = buffer[j];
+        catch (JSONException e) {
+          e.printStackTrace();
+          return;
         }
+        // TODO: Send the speaker to the UI
+      }
+    });
+    super.onConnected();
+  }
 
-        //compute spectrogram
-        double[][] spectrogram = Spectrogram.computeSpectrogram(dBuffer, 100, 50, Window.RECTANGULAR);
+  @Override
+  protected int getNotificationID() {
+    return Constants.NOTIFICATION_ID.AUDIO_SERVICE;
+  }
 
-        //broadcast to UI
-        broadcastSpectrogram(spectrogram);
+  @Override
+  protected String getNotificationContentText() {
+    return getString(R.string.audio_service_notification);
+  }
+
+  @Override
+  protected int getNotificationIconResourceID() {
+    return R.drawable.ic_mic_white_24dp;
+  }
+
+  /**
+   * Broadcasts spectrogram of audio data.
+   *
+   * @param spectrogram 2d array of values
+   */
+  public void broadcastSpectrogram(double[][] spectrogram) {
+    Intent intent = new Intent();
+    intent.putExtra(Constants.KEY.SPECTROGRAM, spectrogram);
+    intent.setAction(Constants.ACTION.BROADCAST_SPECTROGRAM);
+    LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this);
+    manager.sendBroadcast(intent);
+  }
+
+  /**
+   * Called when an audio buffer is received. We compute and visualize the spectrogram
+   * for you.
+   * <br><br>
+   * <p>
+   * Your job is just to send the audio buffer to the server. You should wrap it using
+   * an {@link AudioBufferReading}. For the timestamp, you can pass in any arbitrary
+   * value, or the current system time. It doesn't matter since it won't be used on the
+   * other end, as we aren't live streaming the audio data.
+   *
+   * @param buffer      the raw audio data
+   * @param window_size the size of the buffer
+   * @see MicrophoneRecorder
+   * @see cs.umass.edu.myactivitiestoolkit.audio.MicrophoneRecorder.MicrophoneListener
+   * @see AudioBufferReading
+   */
+  @Override
+  public void microphoneBuffer(short[] buffer, int window_size) {
+    Log.d(TAG, String.valueOf(buffer.length));
+
+    //TODO: Send the audio buffer to the server
+    mClient.sendSensorReading(new AudioBufferReading(
+      mUserID,
+      "MOBILE",
+      "",
+      System.currentTimeMillis(),
+      buffer
+    ));
+
+    //convert short[] to double[] for computing spectrogram
+    double[] dBuffer = new double[buffer.length];
+    for (int j = 0; j < buffer.length; j++) {
+      dBuffer[j] = buffer[j];
     }
+
+    //compute spectrogram
+    double[][] spectrogram = Spectrogram.computeSpectrogram(dBuffer, 100, 50, Window.RECTANGULAR);
+
+    //broadcast to UI
+    broadcastSpectrogram(spectrogram);
+  }
 }
