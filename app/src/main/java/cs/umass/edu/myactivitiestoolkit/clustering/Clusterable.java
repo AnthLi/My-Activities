a6725c1dc83987365763c4fa8d0e37b86b7fab6b
Anthony Li <anthli1895@gmail.com>
2016-11-23 16:41:50 -0500

    Implemented DBScan

--- app/src/main/java/cs/umass/edu/myactivitiestoolkit/clustering/Clusterable.java
+++ app/src/main/java/cs/umass/edu/myactivitiestoolkit/clustering/Clusterable.java
@@ -2,13 +2,15 @@ package cs.umass.edu.myactivitiestoolkit.clustering;
 
 /**
  * Created by snoran on 11/1/15.
- *
+ * <p>
  * This interface defines the {@code distance} method that points must have to be valid for DBScan
  * clustering.
  *
  * @author snoran
  */
 public interface Clusterable<T> {
-    /** Computes the distance between two points - this is generally the Euclidean distance but it need not be */
-    double distance(T other);
+  /**
+   * Computes the distance between two points - this is generally the Euclidean distance but it need not be
+   */
+  double distance(T other);
 }
